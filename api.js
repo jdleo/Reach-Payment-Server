623afb5580af876ce3d2b500c180c59f5eb19ba8
John Leonardo <jdleonardo5@icloud.com>
2019-06-04 13:44:56 -0700

    body parser

--- api.js
+++ api.js
@@ -1,12 +1,19 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 
 //trust proxy
 app.set('trust proxy', true);
 
+//for parsing body
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
 //root
 app.get('/', (req, res) => {
-  res.send('Reach API root.');
+    res.send('Reach API root.');
 });
 
 //API routes for brands and influencers (used by mobile app) 
@@ -14,12 +21,12 @@ app.use('/api/brands', require('./routes/api/brands'));
 
 //google cloud health check
 app.get('/_ah/health', (req, res) => {
-  res.type('text').send('ok');
+    res.type('text').send('ok');
 });
 
 const server = app.listen(8080, () => {
-  const host = server.address().address;
-  const port = server.address().port;
+    const host = server.address().address;
+    const port = server.address().port;
 
-  console.log(`Example app listening at http://${host}:${port}`);
+    console.log(`Example app listening at http://${host}:${port}`);
 });
--- routes/api/brands.js
+++ routes/api/brands.js
@@ -1,46 +1,49 @@
 const stripe = require('stripe')(process.env.STRIPE_PRIVATE_KEY);
 const express = require('express');
 const router = express.Router();
+const bodyParser = require('body-parser');
 
 //set api version
 stripe.setApiVersion("2019-05-16");
 
 //root
 router.get('/', (req, res) => {
-  res.send('api/brands endpoint');
+    res.send('api/brands endpoint');
 });
 
 //for generating ephemeral key for customer
 router.post('/me/ephemeral_keys/', async (req, res, next) => {
-  stripe.ephemeralKeys.create(
-    {customer: req.headers['customer_id']},
-    {stripe_version: "2019-05-16"}
-  ).then((key) => {
-    res.status(200).json(key);
-  }).catch((err) => {
-  	//console.log(err.message);
-    res.status(500).end();
-  });
+    stripe.ephemeralKeys.create({ customer: req.body.customer_id }, { stripe_version: "2019-05-16" }).then((key) => {
+        res.status(200).json(key);
+    }).catch((err) => {
+        //console.log(err.message);
+        res.status(500).end();
+    });
 });
 
 //for generating customer object in Stripe
 router.post('/me/create', async (req, res, next) => {
-  try {
-    stripe.customers.create({
-	  email: req.headers['email']
-	}, function(err, customer) {
-	  // asynchronously called
-	  if (err) {
-	  	//console.log(err);
-	  	res.sendStatus(500);
-	  } else {
-	  	res.send(customer.id);
-	  }
-	});
-  } catch (err) {
-    res.sendStatus(500);
-    next(`Error creating customer object: ${err.message}`);
-  }
+    //check if email parameter is null
+    if (req.body.email) {
+        try {
+            stripe.customers.create({
+                email: req.body.email
+            }, function(err, customer) {
+                // asynchronously called
+                if (err) {
+                    //console.log(err);
+                    res.sendStatus(500);
+                } else {
+                    res.send(customer.id);
+                }
+            });
+        } catch (err) {
+            res.sendStatus(500);
+            next(`Error creating customer object: ${err.message}`);
+        }
+    } else {
+        res.sendStatus(422);
+    }
 });
 
 
