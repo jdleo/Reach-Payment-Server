db3df496cd2459128ac4e2fceae9a534d69c3e1d
John Leonardo <jdleonardo5@icloud.com>
2019-06-05 12:34:31 -0700

    dashboard link

--- routes/api/influencers.js
+++ routes/api/influencers.js
@@ -1,4 +1,4 @@
-const pk = process.env.STRIPE_PRIVATE_KEY;
+const pk = "sk_test_t1rMxmpgLmoCQV4g1p2LfExG0080oRaHN4";
 const stripe = require('stripe')(pk);
 const express = require('express');
 const router = express.Router();
@@ -13,8 +13,35 @@ router.get('/', (req, res) => {
     res.send('api/influencers endpoint');
 });
 
+//for generating dashboard link (Stripe Express account)
+router.post('/me/dashboard', async (req, res, next) => {
+    if (req.body.account_id) {
+        //try to generate dashboard link
+        try {
+            stripe.accounts.createLoginLink(
+                req.body.account_id,
+                function(err, link) {
+                    //asynchronously called
+                    if (err) {
+                        res.sendStatus(500);
+                    } else {
+                        res.status(200).json(link);
+                    }
+                }
+            );
+        } catch (err) {
+            //send 500
+            res.sendStatus(500);
+            next(`Error generating dashboard link: ${err.message}`);
+        }
+    } else {
+        //send 422
+        res.sendStatus(422);
+    }
+});
+
 //for generating customer id from auth token
-router.post('/me/account_id/', async (req, res, next) => {
+router.post('/me/account_id', async (req, res, next) => {
     if (req.body.auth_code) {
         var dataString = `client_secret=${pk}&code=${req.body.auth_code}&grant_type=authorization_code`;
 
@@ -26,15 +53,17 @@ router.post('/me/account_id/', async (req, res, next) => {
 
         function callback(error, response, body) {
             if (!error && response.statusCode == 200) {
+                //send body
                 res.status(200).json(body);
             } else {
-                console.log(response);
+                //send 500
                 res.sendStatus(500);
             }
         }
 
         request(options, callback);
     } else {
+        //send 422
         res.sendStatus(422);
     }
 });
